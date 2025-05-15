e6cc2adc953a5b37472a46f0b7f59bd5f1dc76cc
Tanis Newlun <tanishalfelven@users.noreply.github.com>
2025-05-14 22:29:49 -0600

    Add more emojis

--- core.lua
+++ core.lua
@@ -5,6 +5,20 @@ local emotes = {}
 
 emotes["hff"] = {
 	{ name = ":acow:", file = "acow" },
+	{ name = ":blushcatto:", file = "blushcatto" },
+	{ name = ":bonk:", file = "bonk" },
+	{ name = ":breaduwu:", file = "breaduwu" },
+	{ name = ":cutegun:", file = "cutegun" },
+	{ name = ":gayggles:", file = "gayggles" },
+	{ name = ":noggen:", file = "noggen" },
+	{ name = ":panikcat:", file = "panikcat" },
+	{ name = ":pentagay:", file = "pentagay" },
+	{ name = ":prideheart:", file = "prideheart" },
+	{ name = ":pwease:", file = "pwease" },
+	{ name = ":raccuwu:", file = "raccuwu" },
+	{ name = ":surprisedpikachu:", file = "surprisedpikachu" },
+	{ name = ":transpika:", file = "transpika" },
+	{ name = ":uno:", file = "uno" }
 }
 
 local packageName, addonName = "HFF Emotes", ...
